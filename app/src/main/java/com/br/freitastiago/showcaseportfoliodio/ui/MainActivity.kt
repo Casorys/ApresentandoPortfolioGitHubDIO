f6151dda471106ab732a9c2533bdca1cdd4576ed
Tiago de Freitas | Casorys <tiagofreitas023023@gmail.com>
2021-08-18 15:09:47 -0300

    Primeira Aula
    Nesta primeira aula preparamos o projeto para iniciar o processo de desenvolvimento do App.

--- app/src/main/java/com/br/freitastiago/showcaseportfoliodio/ui/MainActivity.kt
+++ app/src/main/java/com/br/freitastiago/showcaseportfoliodio/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.br.freitastiago.showcaseportfoliodio
+package com.br.freitastiago.showcaseportfoliodio.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.br.freitastiago.showcaseportfoliodio.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

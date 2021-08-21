eebe5873aaa999d9d31b5b4f7a08145c88474b79
Tiago de Freitas | Casorys <tiagofreitas023023@gmail.com>
2021-08-21 08:32:09 -0300

    Aula 4 - Criacao do DataModule
    Nesta aula criamos o Data Module

--- app/src/main/java/com/br/freitastiago/showcaseportfoliodio/App.kt
+++ app/src/main/java/com/br/freitastiago/showcaseportfoliodio/App.kt
@@ -1,6 +1,7 @@
 package com.br.freitastiago.showcaseportfoliodio
 
 import android.app.Application
+import com.br.freitastiago.showcaseportfoliodio.data.di.DataModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,6 +13,9 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
         }
+
+        DataModule.load()
+
     }
 
 }
